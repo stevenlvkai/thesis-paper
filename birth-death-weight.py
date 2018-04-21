3f0c55608ad46a4aa6d2a84a96953a988546d94b
Lv Kai <stevenlvkai@live.com>
2018-04-21 10:44:41 +0800

    Add files via upload

---
+++ birth-death-weight.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 19 11:15:59 2018
+
+@author: 凯
+"""
+
+from random import expovariate
+import matplotlib.pyplot as plt
+from datetime import datetime
+import pandas as pd
+import numpy as np  
+
+from datetime import timedelta
+
+#到达过程
+def birth(average_arrival_interval):
+    
+    #日期
+    d1=[]
+    birthnum = []
+    arrival = 0.0
+    
+    for i in range(150):
+        
+        #泊松过程的时间间隔为参数为λ的指数分布，假设每天发生一次参数为1
+        a=expovariate(1.0 / average_arrival_interval)
+        #平均每*天产生1次分叉
+        arrival += a
+        
+        birthnum.append(1)
+        
+        t=datetime(2017, 8, 1, 0, 0, 0)+timedelta(days=arrival)
+        t=datetime.strftime(t,'%Y-%m-%d')#去除小时、分钟、秒，只留下年月日，方便之后比较秒
+        t=datetime.strptime(t,'%Y-%m-%d')
+        d1.append(t)
+    return birthnum, d1
+#离去过程
+def death(average_leave_interval):
+    
+    #日期
+    d2=[]
+    deathnum = []
+    arrival = 0.0
+    
+    for i in range(150):
+        
+        #泊松过程的时间间隔为参数为λ的指数分布，假设每天发生一次参数为1
+        a=expovariate(1.0 / average_leave_interval)
+        #平均每*天产生1次分叉
+        arrival += a
+        
+        deathnum.append(-1)
+        
+        t=datetime(2017, 9, 1, 0, 0, 0)+timedelta(days=arrival)
+        t=datetime.strftime(t,'%Y-%m-%d')#去除小时、分钟、秒，只留下年月日，方便之后比较秒
+        t=datetime.strptime(t,'%Y-%m-%d')
+        d2.append(t)    
+    return deathnum, d2
+#统一到达与离去过程
+def birth_death(average_arrival_interval, average_leave_interval):
+    birth1, date1 = birth(average_arrival_interval)
+    death1, date2 = death(average_leave_interval)
+    
+    event= birth1 + death1
+    date = date1 + date2
+    
+    data=pd.DataFrame()
+    data['event']=event
+    data['time'] = date
+    data=data.sort_values(by = 'time',ascending = True)#排序，使得到达与离去按时间顺序排列
+    data = data.set_index('time')
+    '''
+    num=[]
+    for i in range(len(data['event'])):
+        e=sum(data['event'].head(i))
+        num.append(e)
+    data['wait_num']=num#累加，计算每个时刻的山寨币数量
+    '''    
+    for m in range(len(data['event'])):#去除被排列到最后的大量离去事件
+        if data['event'][-m-1] != data['event'][-m-2]:
+            n=-m-1
+            break
+    data.drop(data.index[range(300+n,300)],inplace=True)
+    
+    num=[]
+    for i in range(len(data['event'])):
+        e=sum(data['event'].head(i))
+        num.append(e)
+    data['wait_num']=num
+    
+    #计算每个山寨币相对与比特币的价格    
+    weight=[]
+    for i in range(len(data['wait_num'])):
+        if i==0:
+            weight.append(-0.008*np.log(i+0.0001) + 0.0491)#价格公式
+        elif i>0 and i<3:#延迟两次分叉的时间才迎来第二次分叉
+            weight.append(0)
+        else:
+            if data['event'][i-2]>0:
+                weight.append(-0.008*np.log(i-2+0.0001) + 0.0491)
+            else:
+                weight.append(0.008*np.log(i-2+0.0001) - 0.0491)
+    data['weight']=weight
+    #计算每个时刻累计的山寨币价格
+    weight_sum=[]
+    for i in range(len(data['event'])):
+        w=sum(data['weight'].head(i+1))
+        weight_sum.append(w)
+    data['weight_sum']=weight_sum#现存的所有山寨币加起来的价值
+    return data
+
+if __name__=="__main__":
+    import time
+    start_time=time.time()#计时
+    average1=[]
+    average2=[]
+    average3=[]
+    #计算2017、2018、2019年年底的累计山寨币价格
+    for j in range(100):                
+        avg1=0
+        avg2=0
+        avg3=0
+        for i in range(100):    
+            data  = birth_death(7,j+7)
+            a1=data['2017']['weight_sum'].tail(1).values[0]
+            a2=data['2018']['weight_sum'].tail(1).values[0]
+            a3=data['2019']['weight_sum'].tail(1).values[0]
+            avg1=i*avg1/(i+1)+a1/(i+1)
+            avg2=i*avg2/(i+1)+a2/(i+1)
+            avg3=i*avg3/(i+1)+a3/(i+1)
+        average1.append(avg1)
+        average2.append(avg2)
+        average3.append(avg3)
+    inflation=pd.DataFrame()
+    inflation['2017']=average1
+    inflation['2018']=average2
+    inflation['2019']=average3
+    end_time=time.time()
+    time_elapsed=end_time-start_time #in seconds
+    print(time_elapsed)
+    #画图
+    t=range(7,107)
+    a, =plt.plot(t,inflation['2017'])
+    b, =plt.plot(t,inflation['2018'])
+    c, =plt.plot(t,inflation['2019'])
+    plt.legend(handles = [a, b, c], labels = ['2017', '2018', '2019'], loc = 'best')
+    plt.show()
---
+++ birth-death1.py
@@ -0,0 +1,173 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 11 20:40:36 2018
+
+@author: 凯
+"""
+
+from random import expovariate
+import matplotlib.pyplot as plt
+from datetime import datetime
+import pandas as pd
+import numpy as np  
+#import math  
+
+from datetime import timedelta
+#import xlrd
+
+from mpl_toolkits.mplot3d import Axes3D
+
+from matplotlib import cm
+
+def birth(average_arrival_interval):
+    
+    #日期
+    d1=[]
+    arrivals = []
+    bitcoinfork = []
+    arrival = 0.0
+    fork=0
+    
+    for i in range(300):
+        
+        #泊松过程的时间间隔为参数为λ的指数分布，假设每天发生一次参数为1
+        a=expovariate(1.0 / average_arrival_interval)
+        #平均每*天产生1次分叉
+        arrival += a
+        arrivals.append(arrival)
+        
+        fork += 1
+        bitcoinfork.append(fork)
+        
+        t=datetime(2017, 8, 1, 0, 0, 0)+timedelta(days=arrival)
+        t=datetime.strftime(t,'%Y-%m-%d')#去除小时、分钟、秒，只留下年月日，方便之后比较秒
+        t=datetime.strptime(t,'%Y-%m-%d')
+        d1.append(t)
+    #只取每天最后一个数据作为每天的结果
+    new_date = []
+    #new_date1=[]
+    new_num = []
+    for i in range(len(d1)):
+        if i == len(d1) - 1 or d1[i + 1] > d1[i]:  # change to m[i + 1, 0] > m[i, 0]
+            new_num.append(bitcoinfork[i])
+            new_date.append(d1[i])
+            #new_date1.append(datetime.strftime(d1[i],'%Y-%m-%d'))
+    
+    #准备合并观测值和估计值，估计值缺失的数据用后一天的数据填充
+    j=0
+    s=[]
+    for i in range(len(data['date'])):
+        if data['date'][i] <= new_date[j]:
+            s.append(new_num[j])
+        else :   
+            data['date'][i] > new_date[j]
+            j=j+1
+            s.append(new_num[j]) 
+    
+    return s
+def death(average_arrival_interval):
+    
+    #日期
+    d1=[]
+    arrivals = []
+    bitcoinfork = []
+    arrival = 0.0
+    fork=0
+    
+    for i in range(300):
+        
+        #泊松过程的时间间隔为参数为λ的指数分布，假设每天发生一次参数为1
+        a=expovariate(1.0 / average_arrival_interval)
+        #平均每*天产生1次分叉
+        arrival += a
+        arrivals.append(arrival)
+        
+        fork += -1
+        bitcoinfork.append(fork)
+        
+        t=datetime(2017, 10, 1, 0, 0, 0)+timedelta(days=arrival)
+        t=datetime.strftime(t,'%Y-%m-%d')#去除小时、分钟、秒，只留下年月日，方便之后比较秒
+        t=datetime.strptime(t,'%Y-%m-%d')
+        d1.append(t)
+    #只取每天最后一个数据作为每天的结果
+    new_date = []
+    #new_date1=[]
+    new_num = [0]
+    for i in range(len(d1)):
+        if i == len(d1) - 1 or d1[i + 1] > d1[i]:  # change to m[i + 1, 0] > m[i, 0]
+            new_num.append(bitcoinfork[i])
+            new_date.append(d1[i])
+            #new_date1.append(datetime.strftime(d1[i],'%Y-%m-%d'))
+    
+    #准备合并观测值和估计值，估计值缺失的数据用后一天的数据填充
+    j=0
+    e=[]
+    for i in range(len(data['date'])):
+        if data['date'][i] <= new_date[j]:
+            e.append(new_num[j])
+        else :   
+            data['date'][i] > new_date[j]
+            j=j+1
+            e.append(new_num[j]) 
+    
+    return e
+    
+if __name__=="__main__":
+    import time
+    start_time=time.time()#计时
+    
+    average=[]
+    data=pd.DataFrame()
+    date=pd.date_range(start='20170801',periods=1000,freq='1D') #生成日期序列作为index  
+    data['date']=date
+    
+    for j in range(100):
+        avg=0                
+        for i in range(100):    
+            b=birth(7)
+            d=death(j+7)#指不同的服务率
+            
+            #data['birth']=b
+            #data['death']=d
+            #data['remain']=data['birth']+data['death']
+            #last=data['remain'].iloc[-1]
+            #太慢了
+            
+            b_array = np.array(b)
+            d_array = np.array(d)
+            remain = b_array + d_array
+            last = remain[-1]
+            avg=i*avg/(i+1)+last/(i+1)#每种服务率对应的2020年还存在的山寨币的平均数
+        k='{s}'.format(s=j+7)
+        data[k]=remain
+        average.append(avg)
+    data = data.set_index('date')
+    print(average)
+    
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    coefficient=np.arange(7,107,1)
+    time1=np.arange(0,1000,1)
+    time1, coefficient = np.meshgrid(time1, coefficient)
+    Z=data.values
+    Z=np.transpose(Z)
+    surf = ax.plot_surface(time1, coefficient, Z, cmap=cm.coolwarm,
+                           linewidth=0, antialiased=False)
+    
+    xticks = range(0,1200,200)#更换坐标轴的刻度
+    xticklabels = ['2017-08-01','2018-02-17','2018-09-05','2019-03-24','2019-10-10','2020-04-26']    
+    ax.set_xticks(xticks)
+    ax.set_xticklabels(xticklabels,rotation=15)
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Death Interval')
+    ax.set_zlabel('Remaining Cryptocurrency')
+    ax.view_init(elev=20., azim=-50)
+    fig.colorbar(surf, shrink=1, aspect=10)
+    fig.set_size_inches(18.5,10.5)    
+    fig.savefig('D:\吕凯\中央财经大学\毕业论文\生灭过程\power.png', dpi=250)
+    
+    plt.plot(range(7,107),average)
+    
+    end_time=time.time()
+    time_elapsed=end_time-start_time #in seconds
+    print(time_elapsed)
